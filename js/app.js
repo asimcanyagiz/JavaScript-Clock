d73aa56981fde3db77e8a6af82fb1ecd672818b9
Asim Can Yagiz <meycasim@gmail.com>
2022-06-25 11:50:52 +0300

    Body events builded -Functional Background Images

--- js/app.js
+++ js/app.js
@@ -5,10 +5,21 @@ name = document.querySelector("#myName");
 name.innerHTML = prompt("Name?","Asım Can");
 
 //This function for make a real time clock
-function showtime(){
+function showTime(){
     let clock = new Date();
     clock = clock.toLocaleTimeString();
     document.querySelector("#myClock").innerHTML = clock;
-    setTimeout(showtime , 1000);
+    setTimeout(showTime , 1000);
 }
 //MAIN EVENTS - END
+
+//BODY EVENTS - START
+const time = new Date();
+let background = 
+(time.getHours() >= 5 && time.getHours() < 12) ? document.body.style.backgroundImage = "url('/media/morning.jpg')" :
+(time.getHours() >= 12 && time.getHours() < 17) ? document.body.style.backgroundImage = "url('/media/afternoon.jpg')" :
+(time.getHours() >= 17 && time.getHours() < 21) ? document.body.style.backgroundImage = "url('/media/evening.jpg')" :
+(time.getHours() >= 21 || time.getHours() < 5) ? document.body.style.backgroundImage = "url('/media/night.jpg')" :
+"";
+//BODY EVENTS - END
+
